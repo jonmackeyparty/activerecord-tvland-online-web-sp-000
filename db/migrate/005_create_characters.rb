446fa6304e55b49686eb79161d7b65a75a183171
Jon Mackey <jon@jonmackey.party>
2019-02-07 20:18:18 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
-class CreateActors < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration
   def change 
-    create_table :actors do |t|
+    create_table :characters do |t|
       t.string :name 
       t.integer :show_id 
     end 
