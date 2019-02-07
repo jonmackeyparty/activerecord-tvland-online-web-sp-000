b4cd3570553333ecbb5ce04c6d7b314e812de7fb
Jon Mackey <jon@jonmackey.party>
2019-02-07 21:12:16 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_columns_to_shows.rb
+++ db/migrate/009_add_columns_to_shows.rb
@@ -2,7 +2,7 @@ class AddGenresToShows < ActiveRecord::Migration[4.2]
   
   def change 
     add_column :shows, :season, :string 
-    add_column :shows, 
+    add_column :shows, :day, :string
     
   end
 
